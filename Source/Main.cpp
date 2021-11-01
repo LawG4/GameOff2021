8ce8c3087bae4a6b6ae81cee947ef3d857d9bf47
Lawrence <Lawrence.martin.green@gmail.com>
2021-11-01 22:43:40 +0000

    Source : Hello world
    Mainly to test that CMake structure works

---
+++ Source/Main.cpp
@@ -0,0 +1,10 @@
+/*!********************************************************************************************************
+\File          : Main.cpp
+\Copyright     : MIT License
+\Brief         : Adds an entry point into the game
+\Contributors  : Lawrence G,
+ *********************************************************************************************************/
+
+#include <iostream>
+
+int main(int argc, char *argv[]) { std::cout << "Hello World" << std::endl; }
