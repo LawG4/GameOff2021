4f059f2fbf1f320124cca3bea397043ccd7fd6d2
Lawrence <Lawrence.martin.green@gmail.com>
2021-11-02 14:30:13 +0000

    Render: Add basic rendering abstraction

---
+++ Source/Render/DisplayList.cpp
@@ -0,0 +1,6 @@
+/*!********************************************************************************************************
+\File          : DisplayList.cpp
+\Copyright     : MIT License
+\Brief         : Holds the basic display list to render everything this frame
+\Contributors  : Lawrence G,
+ *********************************************************************************************************/
---
+++ Source/Render/Render.cpp
@@ -0,0 +1,6 @@
+/*!********************************************************************************************************
+\File          : Render.cpp
+\Copyright     : MIT License
+\Brief         : Manages the rendering of the current frame handles API abstraction
+\Contributors  : Lawrence G,
+ *********************************************************************************************************/
