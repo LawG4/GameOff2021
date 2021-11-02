4552f02775ce0d11ed591561660a3cfbb07cbe93
Lawrence <Lawrence.martin.green@gmail.com>
2021-11-02 00:47:56 +0000

    Submodule : Add spdlog for logging

--- CMake/Submodules.cmake
+++ CMake/Submodules.cmake
@@ -10,3 +10,13 @@ endif()
 add_subdirectory(${PROJ_ROOT}/External/glfw)
 # Link to the project
 target_link_libraries(${PROJ_NAME} glfw)
+
+# sspdlog
+# Check if the directory exists 
+if(NOT EXISTS ${PROJ_ROOT}/External/spdlog)
+    message(FATAL_ERROR "Spdlog not found, ensure you have downloaded all submodules")
+endif()
+# Add the subdirectory
+add_subdirectory(${PROJ_ROOT}/External/spdlog)
+# Link to the project 
+target_link_libraries(${PROJ_NAME} spdlog)
