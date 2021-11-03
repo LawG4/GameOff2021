927550548acdc7b41238a517b37526e15a3506ad
Lawrence <Lawrence.martin.green@gmail.com>
2021-11-03 00:58:29 +0000

    Remove GL From incoming merge

--- Source/Player/Player.cpp
+++ Source/Player/Player.cpp
@@ -5,16 +5,14 @@
 \Contributors  : Freddie M,
  *********************************************************************************************************/
 
-#include "GL.h"
 #include "Log.h"
 #include "Window.h"
 
 #include <iostream>
 
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods){
-
-        if (key == GLFW_KEY_E && action == GLFW_PRESS) {
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_E && action == GLFW_PRESS) {
         Log.error("button pressed weee");
-        }
-         
     }
+}
