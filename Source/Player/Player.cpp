91086eb55b77f00c52b8220721bea6953b667691
Freddie <mercerfrederick@gmail.com>
2021-11-04 20:20:53 +0000

    Creation of player_class

--- Source/Player/Player.cpp
+++ Source/Player/Player.cpp
@@ -5,29 +5,53 @@
 \Contributors  : Freddie M,
  *********************************************************************************************************/
 
+#include <iostream>
 #include "Log.h"
 #include "Window.h"
 
-#include <iostream>
+/*
+=== Plan ===
+
+Make player class/struct object thing
+- 3 parts (Physical[movements],Actions[attacks, maybe merge with physical??],General updates[e.g health
+updates, all all player data e.g attack pwoer ect])
+
+- Detect when playable, so when paused or un paused
+- Take key commands and
+
+*/
+
+class player
+{  // Player class to hold player data and basic player functions
+   public:
+    int player_health;
+    int player_damage;
+    int player_step_size;
+
+    player()
+    {  // Player constructor to create base player data values
+        player_health = 100;
+        player_damage = 5;
+        player_step_size = 1;
+    }
+};
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
+    player player_class;
+
     if (key == GLFW_KEY_E && action == GLFW_PRESS) {
-        Log.info("button E pressed");
+        Log.info(player_class.player_step_size);
     }
     if (key == GLFW_KEY_W && action == GLFW_PRESS) {
         Log.info("button W pressed");
     }
 }
 
-
-
 /*
 static void cursor_position_callback(GLFWwindow* window, double xpos, double ypos) {
 
         std::cout << "help";
-
-
 }
 
 */
