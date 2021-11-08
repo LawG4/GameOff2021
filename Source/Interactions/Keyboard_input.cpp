3b778ae37b88307bec7a15d716df0ddcb7b671a9
Freddie <mercerfrederick@gmail.com>
2021-11-08 20:14:38 +0000

    Update Keyboard_input.cpp

--- Source/Interactions/Keyboard_input.cpp
+++ Source/Interactions/Keyboard_input.cpp
@@ -12,11 +12,23 @@
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-    if (key == GLFW_KEY_E && action == GLFW_PRESS) {
+    if (key == GLFW_KEY_E && action == GLFW_PRESS) {  // E
         player_class->player_step_size++;
         Log.info(player_class->out_player_damage());
     }
-    if (key == GLFW_KEY_W && action == GLFW_PRESS) {
+    if (key == GLFW_KEY_W && action == GLFW_PRESS) {  // W
         Log.info("button W pressed");
+    }
+    if (key == GLFW_KEY_A && action == GLFW_PRESS) {  // A
+        Log.info("button A pressed");
+    }
+    if (key == GLFW_KEY_A && action == GLFW_PRESS) {  // S
+        Log.info("button S pressed");
+    }
+    if (key == GLFW_KEY_A && action == GLFW_PRESS) {  // D
+        Log.info("button D pressed");
+    }
+    if (key == GLFW_KEY_A && action == GLFW_PRESS) {  // Q
+        Log.info("button Q pressed");
     }
 }
