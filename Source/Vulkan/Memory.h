38ff01902c5e1afe6d5ef3f3820e8fca78ced8c1
Lawrence <Lawrence.martin.green@gmail.com>
2021-11-09 20:05:14 +0000

    Vulkan : Let other vulkan files use the buffer device group utility functionality

--- Source/Vulkan/Memory.h
+++ Source/Vulkan/Memory.h
@@ -22,6 +22,21 @@ struct BufferGroup {
 
 extern std::vector<vk::BufferGroup> bufferStorageMap;
 
+struct DescriptorGroup {
+    std::vector<VkDescriptorSet> descSets;
+    std::vector<BufferGroup> buffers;
+};
+
+extern std::vector<vk::DescriptorGroup> descriptorStorageMap;
+
+/// <summary>Creates a Vulkan buffer and allocates device memory</summary>
+/// <param name="size">The size of the memory to allocate</param>
+/// <param name="usage">The VkBuffer usage flags</param>
+/// <param name="properties">The memory propertys required</param>
+/// <returns>A buffer group which has a buffer and device memory</returns>
+vk::BufferGroup createBufferGroup(VkDeviceSize size, VkBufferUsageFlags usage,
+                                  VkMemoryPropertyFlags properties);
+
 /// <summary> Creates a vertex buffer with the initial contents </summary>
 /// <param name="bufferName"> The name for the buffer in the internal array</param>
 /// <param name="vertexBuffer"> An array of vertices</param>
