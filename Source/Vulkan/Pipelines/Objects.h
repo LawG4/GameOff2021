6d0be664b9b0da03f11078ec6aec60f9a890d417
Lawrence <Lawrence.martin.green@gmail.com>
2021-11-14 22:46:53 +0000

    Vulkan : Declare classes for the new sprite direction

--- Source/Vulkan/Pipelines/Objects.h
+++ Source/Vulkan/Pipelines/Objects.h
@@ -6,6 +6,44 @@
 #include "glm/gtc/matrix_transform.hpp"
 #include "glm/gtx/quaternion.hpp"
 
+/// <summary>
+/// So here's a quick overview of how sprites work. I spoke to some people in the very friendly Vulkan discord
+/// server. It seems that most game engines will track pools and dynamically create a new one when it runs out
+/// of space. However we do not have time to write something like this. Instead what I shall do is use a
+/// descriptor pool per sprite sheet. This I think is the way to minimise descriptor set creation as I think
+/// everything else can go into the vertex buffers
+///
+/// Texture coordinates : Vertex Buffer
+/// MVP Matrix : Push constant
+/// Sprite sheets : passed via descriptor set.
+///
+/// Since we want to be able to put multiple copies of the same sprite multiple times on the screen, we will
+/// provide a structure called a render object. These are the things that the user can actually render
+///
+/// </summary>
+
+class SpriteSheet
+{
+   public:
+    SpriteSheet(const char* TextureFileName);
+    ~SpriteSheet();
+
+   private:
+    /// <summary>We need one pool to allocate everything out of since it's easier to predict than managing a
+    /// global one</summary>
+    VkDescriptorPool pool;
+
+    /// <summary>The layout that we use to stamp together a set</summary>
+    VkDescriptorSetLayout layout;
+
+    /// <summary>The sets that contain the image</summary>
+    std::vector<VkDescriptorSet> sets;
+
+    /// <summary>The sampler for reading the sprite sheet. You never know it could change per sprite
+    /// sheet</summary>
+    VkSampler sampler;
+};
+
 class Sprite
 {
    public:
@@ -38,6 +76,9 @@ class Sprite
     glm::vec3 rot = glm::vec3(0, 0, 0);
 
    protected:
+    /// <summary>Pointer to the sheet being used to draw this sprite</summary>
+    SpriteSheet* sheet;
+
     std::vector<bool> requiresUBOUpdateVector;
 
     /// <summary>Vertex buffer group </summary>
@@ -51,4 +92,22 @@ class Sprite
 
     VkDescriptorPool pool = VK_NULL_HANDLE;
     std::vector<VkDescriptorSet> sets;
+};
+
+class RenderObject
+{
+   public:
+    RenderObject(Sprite* sprite);
+    ~RenderObject();
+    void setPosition(glm::vec3 position);
+    void setRotation(glm::vec3 rotation);
+    void setScale(glm::vec3 scale);
+
+   private:
+    Sprite* _sprite;
+    glm::mat4 _mvp;
+    bool _mvpOutdated;
+    glm::vec3 _pos;
+    glm::vec3 _rot;
+    glm::vec3 _scale;
 };
