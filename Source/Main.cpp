c6efe5f73b81383141d2b087188cbcd4ad147668
Freddie Mercer <mercerfrederick@gmail.com>
2021-11-14 19:33:04 +0000

    Base of UI

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -92,11 +92,20 @@ int main(int argc, char *argv[])
     }
 
     // Create a 2D triangle object
-    const std::vector<glm::vec3> pos = {{0.0f, -0.5f, 0.0f}, {0.5f, 0.5f, 0.0f}, {-0.5f, 0.5f, 0.0f}};
-    const std::vector<glm::vec3> col = {{1.0f, 1.0f, 1.0f}, {0.0f, 1.0f, 0.0f}, {0.0f, 0.0f, 1.0f}};
+    // const std::vector<glm::vec3> pos = {{0.0f, -0.5f, 0.0f}, {0.5f, 0.5f, 0.0f}, {-0.5f, 0.5f, 0.0f}};
+    // const std::vector<glm::vec3> col = {{64.0f, 64.0f, 64.0f}, {64.0f, 64.0f, 64.0f},
+    // {64.0f, 64.0f, 64.0f}};
+    // RenderObject2D *Triangle = new RenderObject2D(pos, col);
+    // RenderObject2D *Triangle2 = new RenderObject2D(pos, col);
+    // float time = 0;
+
+    // Create a 2d square object
+    const std::vector<glm::vec3> pos = {{1.5f, -0.5f, 0.0f}, {1.5f, 0.5f, 0.0f}, {-1.5f, 0.5f, 0.0f}};
+    const std::vector<glm::vec3> pos2 = {{-1.5f, 0.5f, 0.0f}, {-1.5f, -0.5f, 0.0f}, {1.5f, -0.5f, 0.0f}};
+    const std::vector<glm::vec3> col = {{5.0f, 1.0f, 0.0f}, {5.0f, 1.0f, 0.0f}, {5.0f, 1.0f, 0.0f}};
+
     RenderObject2D *Triangle = new RenderObject2D(pos, col);
-    RenderObject2D *Triangle2 = new RenderObject2D(pos, col);
-    float time = 0;
+    RenderObject2D *Triangle2 = new RenderObject2D(pos2, col);
 
     // Key input data
     glfwSetKeyCallback(window, key_callback);
@@ -108,12 +117,12 @@ int main(int argc, char *argv[])
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
 
-        Triangle->rot += glm::vec3(0.0, 0.1, 0.0f);
-        Triangle->scheduleUBOUpdate();
+        // Triangle->rot += glm::vec3(0.0, 0.1, 0.0f);
+        // Triangle->scheduleUBOUpdate();
 
-        Triangle2->pos = glm::vec3(3 * cos(time), 0, 0);
-        Triangle2->scheduleUBOUpdate();
-        time += 0.1f;
+        // Triangle2->pos = glm::vec3(3 * cos(time), 0, 0);
+        // Triangle2->scheduleUBOUpdate();
+        // time += 0.1f;
 
         vk::drawFrame();
     }
