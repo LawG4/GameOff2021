93e004403c198b20a737ad0fac405f4d62f8d1b3
Lawrence <Lawrence.martin.green@gmail.com>
2021-11-24 21:22:19 +0000

    Build : Gameplay loop basics

---
+++ Source/GameplayLoop/Gameplay.cpp
@@ -0,0 +1,8 @@
+/*!********************************************************************************************************
+\File          : Gameplay.cpp
+\Copyright     : GPL-3.0 License
+\Brief         : Provides the gameplay loop so that main doesn't get too clustered
+\Contributors  : Lawrence G,
+ *********************************************************************************************************/
+
+#include "Gameplay.h"
---
+++ Source/GameplayLoop/Gameplay.h
@@ -0,0 +1,6 @@
+/*!********************************************************************************************************
+\File          : Gameplay.h
+\Copyright     : GPL-3.0 License
+\Brief         : Provides the gameplay loop so that main doesn't get too clustered
+\Contributors  : Lawrence G,
+ *********************************************************************************************************/
