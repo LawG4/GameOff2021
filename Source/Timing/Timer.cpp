a1a3f4d197a58d88e67544f35de876b5092bb1cf
Lawrence <Lawrence.martin.green@gmail.com>
2021-11-24 20:17:25 +0000

    Build : Add timer template

---
+++ Source/Timing/Timer.cpp
@@ -0,0 +1,6 @@
+/*!********************************************************************************************************
+\File          : Timer.cpp
+\Copyright     : GPL-3.0 License
+\Brief         : Provides utility for frame times for animation and physics
+\Contributors  : Lawrence G,
+ *********************************************************************************************************/
