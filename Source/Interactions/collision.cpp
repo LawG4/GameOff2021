6f2a6c24c0d41df26e36c5d7a70d2ebf4de07104
Lawrence <Lawrence.martin.green@gmail.com>
2021-11-29 23:59:21 +0000

    Remove strict inequality in the collision detection

--- Source/Interactions/collision.cpp
+++ Source/Interactions/collision.cpp
@@ -26,10 +26,10 @@ bool Collision::pointInBox(glm::vec2 point, const BoundingRect& rect)
 bool Collision::boxInBox(const BoundingRect& rect1, const BoundingRect& rect2)
 {
     // Are the sides of the rectangle touching?
-    if (rect1.topLeft.x + rect1.width >= rect2.topLeft.x &&   // first right edge past second left edge
-        rect1.topLeft.x <= rect2.topLeft.x + rect2.width &&   // first left edge is passed second right
-        rect1.topLeft.y >= rect2.topLeft.y - rect2.height &&  // first top edge is past the bottom
-        rect1.topLeft.y - rect1.height <= rect2.topLeft.y     // first bottom is past second top
+    if (rect1.topLeft.x + rect1.width > rect2.topLeft.x &&   // first right edge past second left edge
+        rect1.topLeft.x < rect2.topLeft.x + rect2.width &&   // first left edge is passed second right
+        rect1.topLeft.y > rect2.topLeft.y - rect2.height &&  // first top edge is past the bottom
+        rect1.topLeft.y - rect1.height < rect2.topLeft.y     // first bottom is past second top
     ) {
         return true;
     } else {
