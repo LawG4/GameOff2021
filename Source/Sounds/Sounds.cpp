ad247f747d7a9435041553cf9ec7030ea2deb6d6
Freddie Mercer <mercerfrederick@gmail.com>
2021-11-29 18:17:45 +0000

    Update Sounds.cpp

--- Source/Sounds/Sounds.cpp
+++ Source/Sounds/Sounds.cpp
@@ -12,7 +12,7 @@ irrklang::ISoundEngine* engine = irrklang::createIrrKlangDevice();
 void Sounds::music()
 {
     // Testing start a sound engine
-    engine->play2D("Sounds/Chill.mp3");
+    engine->play2D("Sounds/Chill.mp3", true);
 }
 
 void Sounds::stopmusic() { engine->drop(); }
